89d860c3abee694a202ae6d99ab744b5d2403c2d
yanghong <yanghong1484944458@gmail.com>
2023-05-03 21:02:28 +0800

    finish

--- test.py
+++ test.py
@@ -13,9 +13,10 @@ from tqdm import tqdm
 from detecter import module_tools
 from torcheval import metrics
 import register
+import pandas
 
 
-def test(model_name: str, use_tpe: bool = False, max_node_count: int = None, prune_node_count: int = None, batch_size: int = 32):
+def test(model_name: str, use_tpe: bool = False, max_node_count: int = None, prune_node_count: int = None, batch_size: int = 32, threshold: float = 0.5):
     logger = logging.getLogger("BCBtest")
     logger.setLevel(logging.DEBUG)
     fh = logging.FileHandler("log/{}.test.log".format(model_name), mode="a+")
@@ -24,7 +25,7 @@ def test(model_name: str, use_tpe: bool = False, max_node_count: int = None, pru
     logger.addHandler(fh)
 
     test_ds = PairCodeset(pandas.read_pickle("dataset/BigCloneBench/data.jsonl.txt.bin"), pandas.read_pickle("dataset/BigCloneBench/test.txt.bin"))
-    test_ds.drop(max_node_count).prune(prune_node_count).sample(30000).use_tpe(use_tpe)
+    test_ds.drop(max_node_count).prune(prune_node_count).use_tpe(use_tpe).sample(50000)
     test_loader = data.DataLoader(
         test_ds, 
         batch_size=batch_size, 
@@ -33,12 +34,12 @@ def test(model_name: str, use_tpe: bool = False, max_node_count: int = None, pru
         collate_fn=collate_fn
     )
 
-    model = module_tools.PretrainModule(model_name).cuda()
+    model = module_tools.get_module(model_name).cuda()
     evaluators: Dict[str, metrics.Metric] = {
-        "f1": metrics.BinaryF1Score(device="cuda"),
-        "precision": metrics.BinaryPrecision(device="cuda"),
-        "recall": metrics.BinaryRecall(device="cuda"),
-        "accuracy": metrics.BinaryAccuracy(device="cuda"),
+        "f1": metrics.BinaryF1Score(device="cuda", threshold=threshold),
+        "precision": metrics.BinaryPrecision(device="cuda", threshold=threshold),
+        "recall": metrics.BinaryRecall(device="cuda", threshold=threshold),
+        "accuracy": metrics.BinaryAccuracy(device="cuda", threshold=threshold),
     }
     pr_curve = metrics.BinaryPrecisionRecallCurve(device="cuda")
 
@@ -56,15 +57,22 @@ def test(model_name: str, use_tpe: bool = False, max_node_count: int = None, pru
                 logger.debug("test: " + ", ".join(["{} {:.4f}".format(key, evaluator.compute()) for key, evaluator in evaluators.items()]))
     print("test: " + ", ".join(["{} {:.4f}".format(key, evaluator.compute()) for key, evaluator in evaluators.items()]))
 
-    # pcurve, rcurve, threshold = pr_curve.compute()
-    # f1curve = 2 / (1 / pcurve + 1 / rcurve)
-    # max_id = f1curve.argmax()
-    # print("f1 {:.4f}, precision {:.4f}, recall {:.4f}, threshold {:.4f}".format(
-    #     f1curve[max_id], pcurve[max_id], rcurve[max_id], threshold[max_id]
-    # ))
+    pcurve, rcurve, threshold = pr_curve.compute()
+    f1curve = 2 / (1 / pcurve + 1 / rcurve)
+    max_id = f1curve.argmax()
+    print("f1 {:.4f}, precision {:.4f}, recall {:.4f}, threshold {:.4f}".format(
+        f1curve[max_id], pcurve[max_id], rcurve[max_id], threshold[max_id]
+    ))
+
+    df = pandas.DataFrame(data={
+        "precision": pcurve.tolist(), 
+        "recall": rcurve.tolist(),
+        "f1": f1curve.tolist(),
+    })
+    df.to_csv("log/prcurve.csv")
 
 
 if __name__ == "__main__":
     # test("BCBdetecter_no_mask")
     # test("BCBdetecter")  
-    test("BCBdetecter_final", use_tpe=True, prune_node_count=1000, batch_size=32)
+    test("BCBdetecter", use_tpe=True, prune_node_count=1280, batch_size=16, threshold=0.76)
